977877a584037209be02189923b597acb79f82ec
Andre Luis da Silveira <andreluis_86@yahoo.com.br>
2018-08-30 17:16:16 -0300

    Resposta dos Exercicios 3 e 4
    As respostas dos exercicios 3 e 4 foram feitas em C#.

---
+++ Execicio3.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios
+{
+    public class Execicio3
+    {
+        int[] numeros { get; set; }
+
+
+        public Execicio3(int numero)
+        {
+            numeros= new int[numero];
+
+        }
+
+        public void  preencherVetor()
+        {
+            Random aleatorio = new Random(); 
+            for(int i=0;i < numeros.Length;i++)
+            {
+
+                numeros[i]=aleatorio.Next(i, 100);
+ 
+             }
+       
+        }
+
+        public void buscaBinariaNormal(int numeroPesquisado)
+        {
+            //inicio,e comeco e final do vetor
+            int comeco = 0;
+            int final = numeros.Length -1;
+            int meio =0;
+            bool achaResultado = false;
+
+
+            while ((comeco <= final)) 
+            {
+                meio = (comeco + final) / 2;
+
+                if(numeroPesquisado == numeros[meio])
+                {
+
+                    
+                    achaResultado=true;
+                    break;
+                }
+                else
+                {
+                    if (numeroPesquisado < numeros[meio])
+                    {
+                        final = meio - 1;
+
+                    }
+                    else
+                    {
+                        comeco = meio + 1;
+                    }
+
+                }
+
+            }
+
+
+            if (achaResultado)
+            {
+                Console.WriteLine($"Foi localizado o registro {numeroPesquisado} na posição " + meio);
+            }
+            else
+            {
+                Console.WriteLine("Registro não foi encontrado");
+            }
+
+        }
+
+        public int buscaBinariaRecursiva(int menor,int maior,int chave)
+        {
+            int media = (maior + menor) / 2;
+            int meio = numeros[media];
+
+            if (menor > maior)
+                return -1;
+            else if (meio == chave)
+                return media;
+            else if (meio < chave)
+                return buscaBinariaRecursiva(media + 1, maior, chave);
+            else
+                return buscaBinariaRecursiva(menor, media-1, chave);
+        }
+
+        public void ordernarVetor()
+        {
+
+            int auxiliar;
+            for (int i=0; i<numeros.Length;i++)
+            {
+                for(int j = 0; j < numeros.Length; j++)
+                {
+                    if(numeros[i] < numeros[j])
+                    {
+                        auxiliar = numeros[i];
+                        numeros[i] = numeros[j];
+                        numeros[j] = auxiliar;
+
+                    }
+
+                }
+            }
+        }
+
+
+        public void mostrarVetor()
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*Exercicio 3*/
+            Console.WriteLine("Digite um numero");
+            string numeroString = Console.ReadLine();
+            int tamanhoDoVetor = Convert.ToInt16(numeroString);
+            Console.WriteLine("\n");
+
+
+           Execicio3 exercicio3 = new Execicio3(tamanhoDoVetor);
+
+            exercicio3.preencherVetor();
+
+            exercicio3.ordernarVetor();
+
+            exercicio3.mostrarVetor();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Digite um numero para fazer uma pesquisa binaria normal.");
+            numeroString = Console.ReadLine();
+            int pesquisarNumero = Convert.ToInt16(numeroString);
+
+            exercicio3.buscaBinariaNormal(pesquisarNumero);
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Digite um numero para fazer uma pesquisa binaria recursiva");
+            numeroString = Console.ReadLine();
+            int pesquisaBinarnariaRecursiva = Convert.ToInt16(numeroString);
+
+            int resultadoPesquisado = exercicio3.buscaBinariaRecursiva(0, tamanhoDoVetor - 1, pesquisaBinarnariaRecursiva);
+
+            if (resultadoPesquisado > 0)
+            {
+                Console.WriteLine($"Foi localizado o registro {pesquisaBinarnariaRecursiva} na posição " + resultadoPesquisado);
+            }
+            else
+            {
+                Console.WriteLine($"Não foi localizado o registro {resultadoPesquisado}");
+            }
+           
+            /*.......................................*/
+
+            /*Exercicio 4*/
+            exercicio4(5);
+
+            Console.ReadLine();
+        }
+
+
+        static void exercicio4(int numero)
+        {
+            if(numero < 0)
+            {
+                return;
+            }
+
+ 
+            int valor = 1;
+            for (int i = 1; i <= numero; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(valor);
+                    valor += 1;  
+                }
+                Console.WriteLine("");
+                    
+            }
+                    }
+    }
+}
